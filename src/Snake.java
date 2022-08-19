15b039f7a79cca23f02fd687a18248aef7782b7a
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2022-08-19 12:59:21 +0200

    Refactor: Remove unnecessary spaces

--- src/Snake.java
+++ src/Snake.java
@@ -6,33 +6,26 @@ public class Snake {
   private int w = Game.width;
   private int h = Game.height;
   private int d = Game.dimension;
-
   private String move; // NOTHING, UP, DOWN, LEFT, RIGHT
 
   public Snake() {
     body = new ArrayList<>();
-
     Rectangle temp = new Rectangle(Game.dimension, Game.dimension);
     temp.setLocation(Game.width / 2 * Game.dimension, Game.height / 2 * Game.dimension);
     body.add(temp);
-
     temp = new Rectangle(d, d);
     temp.setLocation((w / 2 - 1) * d, (h / 2) * d);
     body.add(temp);
-
     temp = new Rectangle(d, d);
     temp.setLocation((w / 2 - 2) * d, (h / 2) * d);
     body.add(temp);
-
     move = "NOTHING";
   }
 
   public void move() {
     if (move != "NOTHING") {
       Rectangle first = body.get(0);
-
       Rectangle temp = new Rectangle(Game.dimension, Game.dimension);
-
       if (move == "UP") {
         temp.setLocation(first.x, first.y - Game.dimension);
       } else if (move == "DOWN") {
@@ -42,7 +35,6 @@ public class Snake {
       } else {
         temp.setLocation(first.x + Game.dimension, first.y);
       }
-
       body.add(0, temp);
       body.remove(body.size() - 1);
     }
@@ -50,9 +42,7 @@ public class Snake {
 
   public void grow() {
     Rectangle first = body.get(0);
-
     Rectangle temp = new Rectangle(Game.dimension, Game.dimension);
-
     if (move == "UP") {
       temp.setLocation(first.x, first.y - Game.dimension);
     } else if (move == "DOWN") {
@@ -62,7 +52,6 @@ public class Snake {
     } else {
       temp.setLocation(first.x + Game.dimension, first.y);
     }
-
     body.add(0, temp);
   }
 
